0e48d41abd33abd20fc59028a1a333f3c60638bb
muskaanbali <37337504+muskaanbali@users.noreply.github.com>
2020-06-07 22:47:08 +0530

    Implementation with Buffered Reader Class instead of Scanner Class

--- src/KadanesAlgorithm.java
+++ src/KadanesAlgorithm.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class KadanesAlgorithm {
     static int max(int a , int b)
@@ -20,17 +24,45 @@ public class KadanesAlgorithm {
         return max_so_far;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner( System.in );
-        System.out.println("Enter size of array: ");
-        int size = sc.nextInt();
-        int arr[] = new int[size];
-        for(int i = 0; i< size; i++ )
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner( System.in );
+//        System.out.println("Enter size of array: ");
+//        int size = sc.nextInt();
+//        int arr[] = new int[size];
+//        for(int i = 0; i< size; i++ )
+//        {
+//            System.out.print("Enter element: ");
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println("Largest possible sum of consecutive subarray is : "+ executeKadanesAlgo(arr, size));
+//
+//    }
+    //Scanner sclass is slow as Compared to Buffered Reader class. If u get TLE on online platforms
+    //we can use the following approach
+    public static void main (String[] args) throws IOException {
+        //code
+        try
         {
-            System.out.print("Enter element: ");
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Largest possible sum of consecutive subarray is : "+ executeKadanesAlgo(arr, size));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int t = Integer.parseInt(br.readLine());
 
+            while( t-- > 0 )
+            {
+                int size = Integer.parseInt(br.readLine());;
+                int arr[] = new int[size];
+                String line = br.readLine();
+                String[] strs = line.trim().split("\\s+");
+                for(int i = 0; i< size; i++ )
+                {
+                    //     System.out.print("Enter element: ");
+                    arr[i] = Integer.parseInt(strs[i]);
+                }
+                System.out.println(executeKadanesAlgo(arr, size));
+            }
+        }
+        catch(Exception e)
+        {
+            //	  e.printStacktrace(System.in)   ;
+        }
     }
 }
